1f4b77e65c2e61ca5cd5477de3cd71dfaaea3626
mitko2703 <mitko2703@abv.bg>
2022-05-14 19:01:11 +0300

    Revert "Remove ignored files"
    This reverts commit 1594a820aa41f407db7469dc6704bfc302eb12b3.

---
+++ PTS_Project_GUI/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace PTS_Project_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ChangeLogsCoursePathDependandButtons(false); //По default деактивираме всички бутони за опции
+            ChangeCourseYear1_2PathDependandButtons(false);
+
+            CollectPaths();
+        }
+
+        private void ChangeLogsCoursePathDependandButtons(bool state)
+        {
+            chestotno_razpredelenie_button.IsEnabled = state;
+            merki_na_centr_tendenciq_button.IsEnabled = state;
+            merki_na_razseivane_button.IsEnabled = state;
+        }
+
+        private void ChangeCourseYear1_2PathDependandButtons(bool state)
+        {
+            chetene_button.IsEnabled = state;
+            korelacionen_analiz_button.IsEnabled = state;
+        }
+
+        private void CollectPaths()
+        {
+            System.Windows.MessageBox.Show("Please select the Logs Course File");
+            Globals.logsCoursePath = GetFilePath();
+
+            if(Globals.logsCoursePath != "") //Ако е избран файл с логове, активираме бутоните, които зависят от него
+            {
+                ChangeLogsCoursePathDependandButtons(true);
+            }else //Ако не е избран ги деактивираме
+            {
+                ChangeLogsCoursePathDependandButtons(false);
+            }
+
+            System.Windows.MessageBox.Show("Please select the Course-A Year 1 File");
+            Globals.courseAYear1Path = GetFilePath();
+
+            System.Windows.MessageBox.Show("Please select the Course-A Year 2 File");
+            Globals.courseAYear2Path = GetFilePath();
+
+            if (Globals.logsCoursePath != "" && Globals.courseAYear1Path != "" && Globals.courseAYear2Path != "") //Ако са избрани всички файлове активираме и останалите 2 бутона
+            {
+                ChangeCourseYear1_2PathDependandButtons(true);
+            }
+            else //Ако дори и един от файловете не е избран, ги оставяме деактивирани
+            {
+                ChangeCourseYear1_2PathDependandButtons(false);
+            }
+        }
+
+        private void ChangePathOfFile(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button btn = (System.Windows.Controls.Button)sender;
+
+            string temp = GetFilePath();
+
+            if(temp != "") //Ако е избран файл (Ако не е натиснат "Cancel" бутона), променяме стойността на съответната глобална променлива
+            {              //и активираме зависимите от нея бутони, защото е възможно при стартирането на програмата да не са избрани никакви файлове и всички бутони да са били деактивирани
+                if (btn.Name == "change_logs_course_path_button")
+                {
+                    Globals.logsCoursePath = temp;
+                    ChangeLogsCoursePathDependandButtons(true);
+                }
+
+                if (btn.Name == "change_course_a_year_1_path_button")
+                {
+                    Globals.courseAYear1Path = temp;
+                }
+
+                if (btn.Name == "change_course_a_year_2_path_button")
+                {
+                    Globals.courseAYear2Path = temp;
+                }
+
+                if (Globals.logsCoursePath != "" && Globals.courseAYear1Path != "" && Globals.courseAYear2Path != "") //Ако са избрани всички файлове активираме и останалите 2 бутона
+                {
+                    ChangeCourseYear1_2PathDependandButtons(true);
+                }
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Nothing is changed!");
+            }
+        }
+
+        private void ReadButtonClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void FrequencyDistributionButtonClick(object sender, RoutedEventArgs e)//Слави
+        {
+            FrequencyDistribution.CalculatingProgram();
+        }
+
+        private void CentralTrendButtonClick(object sender, RoutedEventArgs e) //Богдан
+        {
+            CentralTrend.Calculate();
+        } 
+
+        private void DistractionMeasuresButtonClick(object sender, RoutedEventArgs e) //На мишо
+        {
+            DistractionMeasures.CalculateAndShow();
+        }
+
+        private void CorrelationAnalysisButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public static string GetFilePath()
+        {
+            string filePath = "";
+
+            while (true)
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                fileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+
+
+                DialogResult result = fileDialog.ShowDialog();
+
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    filePath = fileDialog.FileName;
+                    Console.WriteLine(filePath);
+                    fileDialog.Dispose();
+                    break;
+                }
+
+                if(result == System.Windows.Forms.DialogResult.Cancel)
+                {
+                    fileDialog.Dispose();
+                    break;
+                }
+
+                fileDialog.Dispose();
+
+            }
+            return filePath;
+        }
+    }
+
+    static class Globals
+    {
+        public static string logsCoursePath = "";
+        public static string courseAYear1Path = "";
+        public static string courseAYear2Path = "";
+    }
+}
---
+++ Tests/FrequencyDistributionTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using PTS_Project_GUI;
+using System.IO;
+using System.Linq;
+
+namespace Tests
+{
+    public class FrequencyDistributionTests
+    {
+        [Test]
+        public void CopyExcelTableToTempTextFile_TempFilePath_Test()
+        {
+            string test = FrequencyDistribution.CopyExcelTableToTempTextFile("", true);
+
+            string hasToBe = Path.GetTempPath() + "tempExc.txt";
+
+            Assert.AreEqual(hasToBe, test);
+        }
+        [Test]
+        public void AbsolutnaChestota_Test()
+        {
+            List<int> data = new List<int>();
+            
+            data.Add(1);
+            data.Add(2);
+            data.Add(3);
+            data.Add(4);
+            data.Add(4);
+
+            int[] absoluteFR = new int[data.Last()];
+
+            absoluteFR[0] = 1;
+            absoluteFR[1] = 1;
+            absoluteFR[2] = 1;
+            absoluteFR[3] = 2;
+
+            bool[] isLecturePresent = new bool[data.Last()];
+            isLecturePresent[0] = true;
+            isLecturePresent[1] = true;
+            isLecturePresent[2] = true;
+            isLecturePresent[3] = true;
+
+            Assert.AreEqual(absoluteFR,FrequencyDistribution.AbsolutnaChestota(data,4,isLecturePresent));
+        }
+
+        [Test]
+        public void OtnositelnaChestota_Test()
+        {
+            int[] absoluteFR = new int[4];
+
+            absoluteFR[0] = 1;
+            absoluteFR[1] = 1;
+            absoluteFR[2] = 1;
+            absoluteFR[3] = 2;
+
+            double[] relativeFR = new double[4];
+            relativeFR[0] = 20;
+            relativeFR[1] = 20;
+            relativeFR[2] = 20;
+            relativeFR[3] = 40;
+
+            Assert.AreEqual(relativeFR,FrequencyDistribution.OtnositelnaChestota(4,absoluteFR));
+        }
+    }
+}
